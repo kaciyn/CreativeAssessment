573f4bc5f42de32683f9b9e43f5c7c9003fce42d
Kaci <25372015+kaciyn@users.noreply.github.com>
2020-02-29 04:19:13 +0100

    Basic letter grade returner, needs clarification on grade scale

---
+++ CreativeAssessment/CreativeAssessment/Feedback.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreativeAssessment
+{
+    class Feedback
+    {
+        /// <summary>
+        /// Returns the letter grade based on mark.
+        /// </summary>
+        /// <param name="mark">The mark.</param>
+        /// <returns></returns>
+        public string ReturnLetterGrade(float mark)
+        {
+            //TODO need clarification on the actual scale
+            if (mark <= 90)
+            {
+                return "A+";
+
+            }
+            if (mark <= 80)
+            {
+                return "A";
+
+            }
+            if (mark <= 70)
+            {
+                return "A-";
+
+            }
+            if (mark <= 60)
+            {
+                return "B";
+
+            }
+            if (mark <= 50)
+            {
+                return "C";
+            }
+            if (mark <= 40)
+            {
+                return "D+";
+
+            }
+            if (mark <= 30)
+            {
+                return "D";
+
+            }
+            if (mark <= 20)
+            {
+                return "D-";
+
+            }
+            if (mark <= 10)
+            {
+                return "E";
+
+            }
+            return mark <= 0 ? "F" : "NM";
+        }
+
+
+    }
+}
