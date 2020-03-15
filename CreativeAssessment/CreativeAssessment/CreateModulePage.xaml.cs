f2523aa36f7474d9e886a163807effaf0c14eaf3
John G <angereddevotee7@gmail.com>
2020-03-15 01:29:41 +0000

    Added dynamic functionality to the module creation page.

--- CreativeAssessment/CreativeAssessment/CreateModulePage.xaml.cs
+++ CreativeAssessment/CreativeAssessment/CreateModulePage.xaml.cs
@@ -16,5 +16,46 @@ namespace CreativeAssessment
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Method is fired when the the SelectedItem on the AssessmentPicker is changed,
+        /// used to dynamically change the page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnAssessmentPickerChange(object sender, EventArgs e)
+        {
+            if(AssessmentPicker.SelectedItem.ToString() == "1")
+            {
+                Assessment1.IsVisible = true;
+
+                Assessment2.IsVisible = false;
+                Assessment3.IsVisible = false;
+                Assessment4.IsVisible = false;
+            }
+            else if(AssessmentPicker.SelectedItem.ToString() == "2")
+            {
+                Assessment1.IsVisible = true;
+                Assessment2.IsVisible = true;
+
+                Assessment3.IsVisible = false;
+                Assessment4.IsVisible = false;
+            }
+            else if(AssessmentPicker.SelectedItem.ToString() == "3")
+            {
+                Assessment1.IsVisible = true;
+                Assessment2.IsVisible = true;
+                Assessment3.IsVisible = true;
+
+                Assessment4.IsVisible = false;
+            }
+            else if(AssessmentPicker.SelectedItem.ToString() == "4")
+            {
+                Assessment1.IsVisible = true;
+                Assessment2.IsVisible = true;
+                Assessment3.IsVisible = true;
+                Assessment4.IsVisible = true;
+            }
+        }
     }
 }
