7e9c75380f44e6c13af9f417d0707bf4b9aa63ad
Kaci <25372015+kaciyn@users.noreply.github.com>
2020-03-16 20:45:03 +0100

    Gutted MainPage into ClassPage in preparation for merge into development

--- CreativeAssessment/CreativeAssessment/ClassPage.xaml.cs
+++ CreativeAssessment/CreativeAssessment/ClassPage.xaml.cs
@@ -4,7 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,12 +14,75 @@ namespace CreativeAssessment
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ClassPage : ContentPage
     {
+        /// <summary>
+        /// Initialises class list
+        /// </summary>
+        /// <value>
+        /// The class.
+        /// </value>
+        public ObservableCollection<Student> Class { get; private set; }
+
         public ClassPage()
         {
             InitializeComponent();
+            Class = new ObservableCollection<Student>();
 
-            // ListView.ItemsSource = new List<Student>();
+            BindingContext = this;
         }
 
+
+
+        /// <summary>
+        /// Called when [ListView item selected].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SelectedItemChangedEventArgs"/> instance containing the event data.</param>
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Student selectedItem = e.SelectedItem as Student;
+        }
+
+        /// <summary>
+        /// Called when [ListView item tapped].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="ItemTappedEventArgs"/> instance containing the event data.</param>
+        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Student tappedItem = e.Item as Student;
+        }
+
+
+        /// <summary>Opens .</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        async void OnClickOpenCSV(object sender, EventArgs e)
+        {
+            try
+            {
+                FileData filedata = await CrossFilePicker.Current.PickFile();
+                // the dataarray of the file will be found in filedata.DataArray 
+                // file name will be found in filedata.FileName;
+                //etc etc.
+
+                var fileStream = filedata.GetStream();
+                var csvParser = new CsvParser();
+                var parseResults = csvParser.ParseStreamToStudentList(fileStream);
+
+                foreach (var item in parseResults)
+                {
+                    Class.Add(new Student { Marked = item.Result.Marked, Email = item.Result.Email, MatriculationNumber = item.Result.MatriculationNumber, Name = item.Result.Name, Surname = item.Result.Surname });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+        }
+
+
     }
+}
 }
--- CreativeAssessment/CreativeAssessment/MainPage.xaml.cs
+++ CreativeAssessment/CreativeAssessment/MainPage.xaml.cs
@@ -18,72 +18,13 @@ namespace CreativeAssessment
 {
     public partial class MainPage : ContentPage
     {
-        /// <summary>
-        /// Initialises class list
-        /// </summary>
-        /// <value>
-        /// The class.
-        /// </value>
-        public ObservableCollection<Student> Class { get; private set; }
+      
+
         public MainPage()
         {
             InitializeComponent();
-            Class = new ObservableCollection<Student>();
-
-            // AddTestStudents();
 
             BindingContext = this;
-        }
-
-
-        /// <summary>
-        /// Called when [ListView item selected].
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="SelectedItemChangedEventArgs"/> instance containing the event data.</param>
-        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            Student selectedItem = e.SelectedItem as Student;
-        }
-
-        /// <summary>
-        /// Called when [ListView item tapped].
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="ItemTappedEventArgs"/> instance containing the event data.</param>
-        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            Student tappedItem = e.Item as Student;
-        }
-
-
-        /// <summary>Opens .</summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        async void OnClickOpenCSV(object sender, EventArgs e)
-        {
-            try
-            {
-                FileData filedata = await CrossFilePicker.Current.PickFile();
-                // the dataarray of the file will be found in filedata.DataArray 
-                // file name will be found in filedata.FileName;
-                //etc etc.
-
-                var fileStream = filedata.GetStream();
-                var csvParser = new CsvParser();
-                var parseResults = csvParser.ParseStreamToStudentList(fileStream);
-
-                foreach (var item in parseResults)
-                {
-                    Class.Add(new Student { Marked = item.Result.Marked, Email = item.Result.Email, MatriculationNumber = item.Result.MatriculationNumber, Name = item.Result.Name, Surname = item.Result.Surname});
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
         }
 
 
