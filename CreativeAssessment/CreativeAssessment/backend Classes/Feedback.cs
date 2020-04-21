a3bdc5bd3a693c2ebf31aa4b2237a350e0acf631
sectumsemper <kaciy11@yahoo.co.uk>
2020-04-21 17:48:21 +0100

    Fixed grade calculation

--- CreativeAssessment/CreativeAssessment/backend Classes/Feedback.cs
+++ CreativeAssessment/CreativeAssessment/backend Classes/Feedback.cs
@@ -4,74 +4,74 @@ using System.Text;
 
 namespace CreativeAssessment
 {
-    class Feedback
+    public static class Feedback
     {
         /// <summary>
         /// Returns the letter grade based on mark.
         /// </summary>
         /// <param name="mark">The mark.</param>
         /// <returns></returns>
-        public string ReturnLetterGrade(float mark)
+        public static string ReturnLetterGrade(double mark)
         {
             //Functionality for custom grade scales could be easily added here but that's :) outwith :) our purview :)
-            if (mark <= 84.5)
+            if (mark >= 84.5)
             {
                 return "A+";
 
             }
-            if (mark <= 74.49)
+            if (mark >= 74.49)
             {
                 return "A";
 
             }
-            if (mark <= 69.49)
+            if (mark >= 69.49)
             {
                 return "A-";
 
             }
-            if (mark <= 66.49)
+            if (mark >= 66.49)
             {
                 return "B+";
 
             }
-            if (mark <= 62.49)
+            if (mark >= 62.49)
             {
                 return "B";
 
             }
-            if (mark <= 59.49)
+            if (mark >= 59.49)
             {
                 return "B-";
 
             }
-            if (mark <= 56.49)
+            if (mark >= 56.49)
             {
                 return "C+";
             }
-            if (mark <= 52.49)
+            if (mark >= 52.49)
             {
                 return "C";
             }
-            if (mark <= 49.49)
+            if (mark >= 49.49)
             {
                 return "C-";
             }
-            if (mark <= 46.49)
+            if (mark >= 46.49)
             {
                 return "D+";
 
             }
-            if (mark <= 42.49)
+            if (mark >= 42.49)
             {
                 return "D";
 
             }
-            if (mark <= 39.49)
+            if (mark >= 39.49)
             {
                 return "D-";
 
             }
-            return mark <= 30.49 ? "E" : "F";
+            return mark >= 39.49 ? "E" : "F";
         }
 
 
